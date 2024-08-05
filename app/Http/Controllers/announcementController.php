c0f960cda101fe5f1031143fcc74a335a1c5e0ea
jiang <kennykenny19970314@gmail.com>
2024-08-05 15:24:52 +0800

    add try catch exception capture

--- app/Http/Controllers/announcementController.php
+++ app/Http/Controllers/announcementController.php
@@ -8,33 +8,52 @@ use App\Models\announcement_model;
 
 class announcementController extends Controller
 {
+    //取得所有資料
     public function getAllAnnouncements()
     {
-        $result_announcement = announcement_model::with('category')->get();
-        return response()->json(['message' => 'Get announcement successfully', 'get result_announcement content'=> $result_announcement], 201);
+        try{
+            $result_announcement = announcement_model::with('category')->get();
+            
+            return response()->json(['message' => 'Get announcement successfully', 'get result_announcement content'=> $result_announcement], 201);
+        }
+        catch (\Exception $e) {
+            return response()->json([
+                'error' => 'An error occurred.',
+                'message' => $e->getMessage()
+            ], 500);
+        }
     }
-
+    //查詢一筆資料
     public function getAnnouncementById(Request $request, string $id)
     {
-        $result = announcement_model::with('category')->findOrFail($id); 
+        try{
+            $result = announcement_model::with('category')->findOrFail($id); 
 
-        $output = [
-            'id' => $result->id,
-            'announcement_title' => $result->announcement_title,
-            'content' => $result->content,
-            'attachment' => $result->attachment,
-            'image' => $result->image,
-            'stage' => $result->stage,
-            'department' => $result->department,
-            'publish_date' => $result->publish_date,
-            'remove_date' => $result->remove_date,
-            'created_at' => $result->created_at,
-            'updated_at' => $result->updated_at,
-            'announcement_category_id' => $result->category->category_name 
-        ];
-        return response()->json(['message' => 'Get announcement successfully', 'get content'=> $output], 201);
+            $output = [
+                'id' => $result->id,
+                'announcement_title' => $result->announcement_title,
+                'content' => $result->content,
+                'attachment' => $result->attachment,
+                'image' => $result->image,
+                'stage' => $result->stage,
+                'department' => $result->department,
+                'publish_date' => $result->publish_date,
+                'remove_date' => $result->remove_date,
+                'created_at' => $result->created_at,
+                'updated_at' => $result->updated_at,
+                'announcement_category_id' => $result->category->category_name 
+            ];
+            return response()->json(['message' => 'Get announcement successfully', 'get content'=> $output], 201);
+        }
+        catch (\Exception $e) {
+            return response()->json([
+                'error' => 'An error occurred.',
+                'message' => $e->getMessage()
+            ], 500);
+        }
+        
     }
-
+    //新建一筆資料
     public function createAnnouncement(Request $request)
     {
         $validatedData = $request->validate([
@@ -49,10 +68,17 @@ class announcementController extends Controller
             'remove_date' => 'required|date|after:publish_date',
         ]);
 
-        $result = announcement_model::create($validatedData);
-        return response()->json(['message' => 'Announcement created successfully', 'created content'=> $result], 201);
+        try{
+            $result = announcement_model::create($validatedData);
+            return response()->json(['message' => 'Announcement created successfully', 'created content'=> $result], 201);
+        }catch (\Exception $e){
+            return response()->json([
+                'error' => 'An error occurred.',
+                'message' => $e->getMessage()
+            ], 500);
+        }
     }
-
+    //更新一筆資料
     public function updateAnnouncement(Request $request, string $id)
     {
         $validatedData = $request->validate([
@@ -66,19 +92,30 @@ class announcementController extends Controller
             'publish_date' => 'required|date|before_or_equal:remove_date',
             'remove_date' => 'required|date|after:publish_date',
         ]);
-        announcement_model::where('id', $id)->update($validatedData); 
 
-        $result = announcement_model::findOrFail($id);
-        return response()->json(['message' => 'Announcement updated successfully','updated content'=> $result], 201);
+        try{
+            announcement_model::where('id', $id)->update($validatedData); 
+            $result = announcement_model::findOrFail($id);
+            return response()->json(['message' => 'Announcement updated successfully','updated content'=> $result], 201);
+        }catch(\Exception $e){
+            return response()->json([
+                'error' => 'An error occurred.',
+                'message' => $e->getMessage()
+            ], 500);
+        }
+        
     }
-
+    //刪除一筆資料
     public function deleteAnnouncement(Request $request, string $id)
     {
-        announcement_model::where('id',$id)->delete(); 
-
-        // $result = announcement_model::findOrFail($id);
-        return response()->json(['message' => 'Announcement deleted successfully'], 201);
+        try{
+            announcement_model::where('id',$id)->delete(); 
+            return response()->json(['message' => 'Announcement deleted successfully'], 201);
+        }catch(\Exception $e){
+            return response()->json([
+                'error' => 'An error occurred.',
+                'message' => $e->getMessage()
+            ], 500);
+        }
     }
-
-
 }
