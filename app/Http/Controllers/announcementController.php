5011c88118036be4460b489541ca56bd73961502
jiang <kennykenny19970314@gmail.com>
2024-08-05 08:05:33 +0800

    add getall announcement data api & add creating a raw of announcement data api

--- app/Http/Controllers/announcementController.php
+++ app/Http/Controllers/announcementController.php
@@ -4,7 +4,31 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\announcement_model;
+
 class announcementController extends Controller
 {
-    //
+    public function getAllAnnouncements()
+    {
+        return announcement_model::all();
+    }
+
+    public function createAnnouncement(Request $request)
+    {
+        $validatedData = $request->validate([
+            'announcement_title' => 'required|string|max:255',
+            'content' => 'required|string', 
+            'attachment' => 'nullable|string|max:255', 
+            'image' => 'nullable|string|max:255', 
+            'stage' => 'required|boolean',
+            'announcement_category_id' => 'required|integer',
+            'department_id' => 'required|integer|',
+            'publish_date' => 'required|date|before_or_equal:remove_date',
+            'remove_date' => 'required|date|after:publish_date',
+        ]);
+
+        $result = announcement_model::create($validatedData);
+        return response()->json(['message' => 'Announcement created successfully'], 201);
+            
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\announcementController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/announcement',[ announcementController::class,'createAnnouncement']);
+Route::get('/announcement',[ announcementController::class,'getAllAnnouncements']);
