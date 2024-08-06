92d262c62c946887341a3e358b2e481f12b8de98
jiang <kennykenny19970314@gmail.com>
2024-08-06 11:08:49 +0800

    update validator of   createAnnouncement and updateAnnouncement

--- app/Http/Controllers/announcementController.php
+++ app/Http/Controllers/announcementController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 use App\Models\announcement_model;
 
@@ -56,19 +57,61 @@ class announcementController extends Controller
     //新建一筆資料
     public function createAnnouncement(Request $request)
     {
-        $validatedData = $request->validate([
-            'announcement_title' => 'required|string|max:255',
-            'content' => 'required|string', 
-            'attachment' => 'nullable|string|max:255', 
-            'image' => 'nullable|string|max:255', 
-            'stage' => 'required|boolean',
-            'announcement_category_id' => 'required|integer',
-            'department' => 'required|string|max:255',
-            'publish_date' => 'required|date|before_or_equal:remove_date',
-            'remove_date' => 'required|date|after:publish_date',
-        ]);
+        // $validatedData = $request->validate([
+        //     'announcement_title' => 'required|string|max:255',
+        //     'content' => 'required|string', 
+        //     'attachment' => 'nullable|string|max:255', 
+        //     'image' => 'nullable|string|max:255', 
+        //     'stage' => 'required|boolean',
+        //     'announcement_category_id' => 'required|integer',
+        //     'department' => 'required|string|max:255',
+        //     'publish_date' => 'required|date|before_or_equal:remove_date',
+        //     'remove_date' => 'required|date|after:publish_date',
+        // ]);
+        $validator = Validator::make(
+            [
+                'announcement_title'=>$request->announcement_title,
+                'content'=>$request->content,
+                'attachment'=>$request->attachment,
+                'image'=>$request->image,
+                'stage'=>$request->stage,
+                'announcement_category_id'=>$request->announcement_category_id,
+                'department'=>$request->department,
+                'publish_date'=>$request->publish_date,
+                'remove_date'=>$request->remove_date,
+                //也可以用$request->all()
+            ],
+            [
+                'announcement_title' => 'required|string|max:255',
+                'content' => 'required|string', 
+                'attachment' => 'nullable|string|max:255', 
+                'image' => 'nullable|string|max:255', 
+                'stage' => 'required|boolean',
+                'announcement_category_id' => 'required|integer',
+                'department' => 'required|string|max:255',
+                'publish_date' => 'required|date|before_or_equal:remove_date',
+                'remove_date' => 'required|date|after:publish_date',
+            ],
+            [
+                'announcement_title.string' => '【標題】需為字串',
+                'content.string' => '【內容】需為字串',
+                'attachment.string' => '【附件】需為字串',
+                'image.string' => '【圖片】需為字串',
+                'stage.boolean' => '【狀態】需為布林值',
+                'announcement_category_id.integer' => '【分類】需為整數',
+                'department.string' => '【單位】需為字串',
+                'publish_date.date' => '【發布日】需為日期',
+                'remove_date.date' => '【預定移除日】需為日期',
+            ]
+        );
+        //驗證沒通過就結束
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);;
+        }
 
+        //重新調閱request body進行資料處理
         try{
+            $validatedData =  $request->all();
             $result = announcement_model::create($validatedData);
             return response()->json(['message' => 'Announcement created successfully', 'created content'=> $result], 201);
         }catch (\Exception $e){
@@ -81,19 +124,61 @@ class announcementController extends Controller
     //更新一筆資料
     public function updateAnnouncement(Request $request, string $id)
     {
-        $validatedData = $request->validate([
-            'announcement_title' => 'required|string|max:255',
-            'content' => 'required|string', 
-            'attachment' => 'nullable|string|max:255', 
-            'image' => 'nullable|string|max:255', 
-            'stage' => 'required|boolean',
-            'announcement_category_id' => 'required|integer',
-            'department' => 'required|string|max:255',
-            'publish_date' => 'required|date|before_or_equal:remove_date',
-            'remove_date' => 'required|date|after:publish_date',
-        ]);
+        // $validatedData = $request->validate([
+        //     'announcement_title' => 'required|string|max:255',
+        //     'content' => 'required|string', 
+        //     'attachment' => 'nullable|string|max:255', 
+        //     'image' => 'nullable|string|max:255', 
+        //     'stage' => 'required|boolean',
+        //     'announcement_category_id' => 'required|integer',
+        //     'department' => 'required|string|max:255',
+        //     'publish_date' => 'required|date|before_or_equal:remove_date',
+        //     'remove_date' => 'required|date|after:publish_date',
+        // ]);
+        $validator = Validator::make(
+            [
+                'announcement_title'=>$request->announcement_title,
+                'content'=>$request->content,
+                'attachment'=>$request->attachment,
+                'image'=>$request->image,
+                'stage'=>$request->stage,
+                'announcement_category_id'=>$request->announcement_category_id,
+                'department'=>$request->department,
+                'publish_date'=>$request->publish_date,
+                'remove_date'=>$request->remove_date,
+                //也可以用$request->all()
+            ],
+            [
+                'announcement_title' => 'required|string|max:255',
+                'content' => 'required|string', 
+                'attachment' => 'nullable|string|max:255', 
+                'image' => 'nullable|string|max:255', 
+                'stage' => 'required|boolean',
+                'announcement_category_id' => 'required|integer',
+                'department' => 'required|string|max:255',
+                'publish_date' => 'required|date|before_or_equal:remove_date',
+                'remove_date' => 'required|date|after:publish_date',
+            ],
+            [
+                'announcement_title.string' => '【標題】需為字串',
+                'content.string' => '【內容】需為字串',
+                'attachment.string' => '【附件】需為字串',
+                'image.string' => '【圖片】需為字串',
+                'stage.boolean' => '【狀態】需為布林值',
+                'announcement_category_id.integer' => '【分類】需為整數',
+                'department.string' => '【單位】需為字串',
+                'publish_date.date' => '【發布日】需為日期',
+                'remove_date.date' => '【預定移除日】需為日期',
+            ]
+        );
+        //驗證沒通過就結束
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);;
+        }
 
+        //重新調閱request body進行資料處理
         try{
+            $validatedData =  $request->all();
             announcement_model::where('id', $id)->update($validatedData); 
             $result = announcement_model::findOrFail($id);
             return response()->json(['message' => 'Announcement updated successfully','updated content'=> $result], 201);
