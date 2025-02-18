a7167f3dec70d71b2687a50acaa183f96a0bcae0
10392姜鐙淯 <kennykenny19970314@gmail.com>
2025-02-18 16:34:46 +0800

    Reapply "feat: Add tables News and ThreeLights"
    This reverts commit 19d5dd29464d4a10cda286e5d9555255b4b7dddd.

---
+++ database/migrations/2025_02_08_021741_create_news_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->date('event_date');
+            $table->time('event_time');
+            $table->text('description')->nullable();
+            $table->text('remark')->nullbal();
+            $table->date('create_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('news');
+    }
+};
