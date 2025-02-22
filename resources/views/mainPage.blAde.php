822d27b3ebc84f760dec2111cecdbfff2bac85ef
kennykenny19970314@gmail.com <kennykenny19970314@gmail.com>
2025-02-22 20:32:27 +0800

    feat: modify mainPage content

--- resources/views/mainPage.blAde.php
+++ resources/views/mainPage.blAde.php
@@ -5,6 +5,6 @@
 @section('content')
     <div class="container">
         <h1>Welcome to the Main Page</h1>
-        <p>This page does not require any parameters.</p>
+        <p>Here is the bulletinboard web app.</p>
     </div>
 @endsection
